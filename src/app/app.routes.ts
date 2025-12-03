17e63f82905b91026e06f7d7436a08806158fe86
sakjanonkk <sakjanon.k@kkumail.com>
2025-12-04 00:02:46 +0700

    feat: added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,16 @@ export const routes: Routes = [
     pathMatch: 'full',
     redirectTo: 'login',
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./features/auth/login/login.route').then((m) => m.LoginRoutes),
+  },
+  {
+    path: 'vehicle',
+    loadChildren: () => import('./features/vehicle/vehicle.route').then((m) => m.VehicleRoutes),
+  },
+  {
+    path: 'staff',
+    loadChildren: () => import('./features/staff/staff.route').then((m) => m.StaffRoutes),
+  }
 ];
